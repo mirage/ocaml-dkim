6669c326df169bc9c269578f3d55cf7fd791638a
Calascibetta Romain <romain.calascibetta@gmail.com>
2024-05-31 10:26:20 +0200

    Upgrade dkim-mirage with dns-client-mirage.8.0.0

--- mirage/dkim_mirage.mli
+++ mirage/dkim_mirage.mli
@@ -1,31 +1,16 @@
 type 'a stream = unit -> 'a option Lwt.t
 
-module Make
-    (R : Mirage_random.S)
-    (T : Mirage_time.S)
-    (C : Mirage_clock.MCLOCK)
-    (P : Mirage_clock.PCLOCK)
-    (S : Tcpip.Stack.V4V6) : sig
+module Make (P : Mirage_clock.PCLOCK) (D : Dns_client_mirage.S) : sig
   type nameserver =
     [ `Plaintext of Ipaddr.t * int | `Tls of Tls.Config.client * Ipaddr.t * int ]
 
   val server :
-    S.t ->
-    ?cache_size:int ->
-    ?edns:[ `Auto | `Manual of Dns.Edns.t | `None ] ->
-    ?nameservers:Dns.proto * nameserver list ->
-    ?timeout:int64 ->
-    'a Dkim.dkim ->
-    (Dkim.server, [> `Msg of string ]) result Lwt.t
+    D.t -> 'a Dkim.dkim -> (Dkim.server, [> `Msg of string ]) result Lwt.t
 
   val verify :
     ?newline:Dkim.newline ->
-    ?cache_size:int ->
-    ?edns:[ `Auto | `Manual of Dns.Edns.t | `None ] ->
-    ?nameservers:Dns.proto * nameserver list ->
-    ?timeout:int64 ->
     (string * int * int) stream ->
-    S.t ->
+    D.t ->
     ( Dkim.signed Dkim.dkim list * Dkim.signed Dkim.dkim list,
       [> `Msg of string ] )
     result
